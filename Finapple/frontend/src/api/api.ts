6b6244c2320254675eb56ebbfc3a2648a07e3332
pritamscodee <codewithpritam29@gmail.com>
2026-05-14 19:27:47 +0530

    railway to render

--- Finapple/frontend/src/api/api.ts
+++ Finapple/frontend/src/api/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://finapple-production.up.railway.app",
+  baseURL: "https://finapple-8f21.onrender.com",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
